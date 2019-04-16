5b1a29b50b398a6be8531a1ede833f53981c466f
FilipGrudzinski <grudzinski.filip@gmail.com>
2019-04-16 18:51:14 +0200

    ListScreen TableVIew added, MVC Pattern used

---
+++ OmniNews/View/Cells/ListScreenCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListScreenCell.swift
+//  OmniNews
+//
+//  Created by Filip on 16/04/2019.
+//  Copyright © 2019 Filip. All rights reserved.
+//
+
+import UIKit
+
+class ListScreenCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
