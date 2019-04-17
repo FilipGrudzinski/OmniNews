987fff63117040eaf24b81e124fcdf2423bbe19b
FilipGrudzinski <grudzinski.filip@gmail.com>
2019-04-17 19:15:47 +0200

    models added

---
+++ OmniNews/Model/ArticleModel.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleModel.swift
+//  OmniNews
+//
+//  Created by Filip on 17/04/2019.
+//  Copyright © 2019 Filip. All rights reserved.
+//
+
+import Foundation
+
+struct ArticleModel {
+    let articleTitle: String
+    let articleImageID: String
+}
+
+
---
+++ OmniNews/Model/TopicModel.swift
@@ -0,0 +1,14 @@
+//
+//  TopicModel.swift
+//  OmniNews
+//
+//  Created by Filip on 17/04/2019.
+//  Copyright © 2019 Filip. All rights reserved.
+//
+
+import Foundation
+
+struct TopicModel {
+    let topicTitle: String
+    let topicType: String
+}
